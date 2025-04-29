04df0721f2934f17d7ced31a753a45caf0f868fc
ujwals2003 <ujwalskumar8@gmail.com>
2025-04-29 15:26:02 +0530

    29-04-2025

---
+++ IPL_word_count/src/Rcb.java
@@ -0,0 +1,15 @@
+public class Rcb {
+    public static void main(String[] args)
+    {
+        String match = "RCB WON AGAINST CSK MATCH";
+        String [] words = match.split(" ");
+        for(String Words : words){
+            System.out.print( Words.length() + " ");
+        }
+
+
+
+
+
+    }
+}
