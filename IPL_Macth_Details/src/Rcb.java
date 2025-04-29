b444b49f22be14e0f1105d7d1fbdb02b4bce400b
ujwals2003 <ujwalskumar8@gmail.com>
2025-04-29 15:12:06 +0530

    29-04-2025

---
+++ IPL_Macth_Details/src/Rcb.java
@@ -0,0 +1,7 @@
+public class Rcb {
+    public static void main(String[] args) {
+        String match = "RCB WILL WIN MACTH TOMMWOR";
+        String sentence1 = match.replace("WIN MACTH TOMMWOR", "NIW HCTAM ROWMMOT");
+        System.out.println("The match won by: " + sentence1);
+    }
+}
