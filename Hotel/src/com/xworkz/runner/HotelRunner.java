e42b52cbc5d2fdd13abed4fa57b047d752806c9c
ujwals2003 <ujwalskumar8@gmail.com>
2025-06-10 22:34:54 +0530

    exception 10

---
+++ Hotel/src/com/xworkz/runner/HotelRunner.java
@@ -0,0 +1,16 @@
+package com.xworkz.runner;
+
+import com.xworkz.ujwal.HotelException;
+
+public class HotelRunner {
+    public static void main(String[] args) {
+        int availableRooms = 0;
+        int requestedRooms = 2;
+
+        if(availableRooms >= requestedRooms) {
+            System.out.println("Rooms are available for booking");
+        } else {
+            throw new HotelException("No rooms available for booking");
+        }
+    }
+}
---
+++ Hotel/src/com/xworkz/ujwal/HotelException.java
@@ -0,0 +1,11 @@
+package com.xworkz.ujwal;
+
+public class HotelException extends RuntimeException {
+    public HotelException() {
+        super();
+    }
+
+    public HotelException(String message) {
+        super(message);
+    }
+}
